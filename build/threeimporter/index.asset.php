7944bea7b2597f16e6ed595c69416d8b12906763
callahan-codes <bryceccode@gmail.com>
2025-09-02 08:35:54 -0500

    final touches - threejs icon for block icon

--- build/threeimporter/index.asset.php
+++ build/threeimporter/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '593ef569c0f2fed245f6');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '6dcf3db5ef499f6d4a3e');
--- src/threeimporter/index.js
+++ src/threeimporter/index.js
@@ -4,7 +4,33 @@ import Edit from './edit';
 import Save from './save';
 import metadata from './block.json';
 
-registerBlockType( metadata.name, {
+const customIcon = (
+	<svg
+		viewBox="0 0 226.77 226.77"
+		fill="none"
+		xmlns="http://www.w3.org/2000/svg"
+	>
+		<g
+			transform="translate(8.964 4.2527)"
+			fillRule="evenodd"
+			stroke="#000"
+			strokeLinecap="butt"
+			strokeLinejoin="round"
+			strokeWidth="4"
+		>
+			<path d="M63.02 200.61L19.807 25.67l173.23 49.874z" />
+			<path d="M106.39 50.612l21.591 87.496-86.567-24.945z" />
+			<path d="M84.91 125.03l-10.724-43.465 43.008 12.346z" />
+			<path d="M63.458 38.153l10.724 43.465-43.008-12.346z" />
+			<path d="M149.47 62.93l10.724 43.465-43.008-12.346z" />
+			<path d="M84.915 125.06l10.724 43.465-43.008-12.346z" />
+		</g>
+	</svg>
+);
+
+registerBlockType(metadata.name, {
+	...metadata,
+	icon: customIcon,
 	edit: Edit,
 	save: Save,
-} );
+});
